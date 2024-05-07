dd892efb6bc8a23a252cbb07acb7f01e150303fa
vcks101 <vickypedrozo24@gmail.com>
2024-05-07 10:50:00 -0500

    first commit

---
+++ app-api-esvelt/src/App.svelte
@@ -0,0 +1,40 @@
+<script>
+  import Character from "./lib/character.svelte";
+
+
+  let characters = [];
+  let page = 1;
+
+  async function loadCharacters() {
+    const response = await fetch(
+      "https://rickandmortyapi.com/api/character?page=" + page,
+    );
+    const data = await response.json();
+    characters = data.results;
+  }
+  loadCharacters();
+
+  function nextPage() {
+    page++;
+    loadCharacters();
+  }
+
+  function previousPage() {
+    page--;
+    loadCharacters();
+  }
+</script>
+
+<h1 class="title">Rick and Morty Svelte</h1>
+
+<div class="container">
+  <div class="btns">
+    <button class="btn" on:click={previousPage} disabled={page === 1}>REGRESAR</button>
+    <button class="btn" on:click={nextPage}>SIGUIENTE</button>
+  </div>
+ <div class="grid">
+  {#each characters as character}
+    <Character {character} />
+  {/each}
+ </div>
+</div>
---
+++ app-api-esvelt/src/lib/character.svelte
@@ -0,0 +1,10 @@
+<script>
+    export let character;
+</script>
+
+
+
+<div class="character">
+    <img src={character.image} alt={character.name} />
+    <h1>{character.name}</h1>
+  </div>
